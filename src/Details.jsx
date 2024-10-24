bc48cc0cfe89daedba254da480ac39d648f46325
Christian Esparza <cesparzajp@gmail.com>
2024-10-24 18:16:02 +0900

    feat: fetch resume

--- src/Details.jsx
+++ src/Details.jsx
@@ -2,6 +2,17 @@ import SocialLink from "./SocialLink.jsx"
 import './Details.css'
 
 function Details(){
+    
+    const apiRootUrl = "http://localhost:8080";
+
+    const handleClick = (url) => {
+        try{
+            window.open(`${apiRootUrl}${url}`,'_blank');
+        }catch(error){
+            console.log("Error fetching resume", error);
+        }
+    }
+
     const socialLinks = [
         {
             href: "https://github.com/Durounseki",
@@ -12,20 +23,24 @@ function Details(){
             faClass: "fa-brands fa-linkedin",
         }
     ]
+
     return (
         <>
             <header>
                 <h1 className="name">Esparza Lopez</h1>
                 <p className="role">Full Stack Developer</p>
                 <p className="contact">christian@esparzalopez.com</p>
+                <p className="location"><i className="fa-solid fa-location-dot"></i> Japan</p>
                 <div className="social-links">
                     {socialLinks.map((link, index) => (
                         <SocialLink key={index} link={link}/>
                     ))}
+                    <a className="resume" href="#" onClick={()=> handleClick("/api/resume")}>
+                        <i className="fa-solid fa-file-lines"></i><p>Resume</p>
+                    </a>
                 </div>
             </header>
         </>
     )
 }
-
 export default Details;
