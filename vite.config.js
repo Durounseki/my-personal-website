047c66aeac32fa5268fe2f6ba5d98416a5708d83
Durounseki <156300780+Durounseki@users.noreply.github.com>
2024-12-22 19:37:17 +0900

    fix: use env variable for api url

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy:{
-      '/resume':{
-        target: 'http://localhost:8080/api',
-        changeOrigin: true
+export default defineConfig(({command, mode}) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+
+    plugins: [
+      react(),
+    ],
+    server: {
+      proxy:{
+        '/resume':{
+          target: `${env.VITE_API_ROOT_URL}/api`,
+          changeOrigin: true
+        }
       }
     }
   }
