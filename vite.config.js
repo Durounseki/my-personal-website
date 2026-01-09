d608a69b998b33a159c6936116bbbcb820bc5c16
Durounseki <156300780+Durounseki@users.noreply.github.com>
2026-01-09 15:06:09 +0900

    chore: install tanstack router

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,26 @@
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 
 // https://vite.dev/config/
-export default defineConfig(({command, mode}) => {
-  const env = loadEnv(mode, process.cwd(), '');
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
 
   return {
-
     plugins: [
+      tanstackRouter({
+        target: "react",
+        autoCodeSplitting: true,
+      }),
       react(),
     ],
     server: {
-      proxy:{
-        '/resume':{
+      proxy: {
+        "/resume": {
           target: `${env.VITE_API_ROOT_URL}/api`,
-          changeOrigin: true
-        }
-      }
-    }
-  }
-})
+          changeOrigin: true,
+        },
+      },
+    },
+  };
+});
